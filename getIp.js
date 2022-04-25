f732991c14552009d6d46c606f8a75c7592c39b5
Albert Strzyzewski <albert.strzyzewski@rb.com>
2022-04-25 19:02:12 +0200

    Refaktoryzacja funkcji getIp

--- getIp.js
+++ getIp.js
@@ -2,12 +2,14 @@ import publicIp from 'public-ip'
 import ipaddrJs from 'ipaddr.js'
 
 const getIp = async (address) => {
-    const ip = (((ip) => (ip.isIPv4MappedAddress() ? (p.toIPv4Address()) : (ip)))(ipaddrJs.parse(address)));
-    return (
-        ["unspecified","broadcast","linkLocal","loopback","private","reserved"].includes(ip.range()) 
-        ?
-        (await Promise.any([publicIp.v4(), publicIp.v6()])) : (ip.toString())
-    );
+    let ret;
+    const ipAddress = ((address => address?.isIPv4MappedAddress?.() ? address.toIPv4Address() : address)(ipaddrJs.parse(address)));
+    if(["broadcast","linkLocal","loopback","private","reserved","unspecified"].includes(ipAddress.range())){
+        ret = await Promise.any([publicIp.v4(), publicIp.v6()])
+    }else{
+        ret = ipAddress.toString()
+    }
+    return ret;
 };
 
 
